0994f38ef868b51a12de1fe70db0adc2a3568f3b
jang <treous11@gmail.com>
2024-09-20 20:35:44 +0900

    커밋

---
+++ src/main/java/com/example/MeongdaySTS/MeongdayStsApplication.java
@@ -0,0 +1,13 @@
+package com.example.MeongdaySTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeongdayStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeongdayStsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MeongdaySTS/MeongdayStsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MeongdaySTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeongdayStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
