9ba0846001bb197709a4e79e1c219e38e0036c4f
kimys <kimyslive7@naver.com>
2024-10-07 21:50:29 +0900

    dto <- ->entity
    시범 push

--- src/main/java/com/example/MeongdaySTS/product/entity/Product.java
+++ src/main/java/com/example/MeongdaySTS/product/entity/Product.java
@@ -12,21 +12,25 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 
-public class productEntity extends BaseEntity {
+public class Product extends BaseEntity {
 
     @Id     //기본키
     @GeneratedValue(strategy = GenerationType.IDENTITY) //JPA에서 기본 키의 값을 자동으로 생성하도록 지시
     int productNo;  //상품 고유번호
 
-    @Column(length = 10)
+    @Column(length = 20, nullable = false)
     String productName; // 상품명
 
+    @Column(length = 10, nullable = false)
     int productPrice;   //상품가격
 
+    @Column(length = 100, nullable = false)
     String productThembnail;    //썸네일
 
+    @Column(length = 10, nullable = false)
     String productDetailImage;  //상품이미지
 
+    @Column(length = 10, nullable = false)
     String productCategory;     //상품카테고리
 
 }
---
+++ src/main/java/com/example/MeongdaySTS/product/service/ProductService.java
@@ -0,0 +1,45 @@
+package com.example.MeongdaySTS.product.service;
+
+import com.example.MeongdaySTS.product.dto.ProductDTO;
+import com.example.MeongdaySTS.product.entity.Product;
+
+public interface ProductService {
+
+
+
+
+    default Product dtoToEntity(ProductDTO dto){
+
+        Product entity = Product.builder()
+                .productNo(dto.getProductNo())
+                .productName(dto.getProductName())
+                .productPrice(dto.getProductPrice())
+                .productThembnail(dto.getProductThembnail())
+                .productDetailImage(dto.getProductDetailImage())
+                .productCategory(dto.getProductCategory())
+                .build();
+
+        return entity;
+    }
+
+    default ProductDTO entityToDto(Product entity){
+
+        ProductDTO dto = ProductDTO.builder()
+                .productNo(entity.getProductNo())
+                .productNo(entity.getProductNo())
+                .productName(entity.getProductName())
+                .productPrice(entity.getProductPrice())
+                .productThembnail(entity.getProductThembnail())
+                .productDetailImage(entity.getProductDetailImage())
+                .productCategory(entity.getProductCategory())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+
+        return dto;
+    }
+
+}
+
+
+
